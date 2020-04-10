642e7259330d0835e5b6d344d3bc9b514148eb7f
Andreas Stocker <AStocker@anexia-it.com>
2020-04-10 13:26:06 +0200

    Initial commit

---
+++ main.php
@@ -0,0 +1,26 @@
+<?php
+require_once("./functions.php");
+
+function entrypoint($event, $context) {
+    $a = $event->data->a;
+    $b = $event->data->b;
+
+    echo "Hello from my git repository!\n";
+    echo "Calculating with numbers '$a' and '$b'.\n";
+    echo "…\n";
+
+    return [
+        'status' => 200,
+        'response_headers' => [
+            'x-calculation-param-1' => $a,
+            'x-calculation-param-2' => $b,
+        ],
+        'data' => [
+            'sum' => add($a, $b),
+            'difference' => subtract($a, $b),
+            'product' => multiply($a, $b),
+            'quotient' => devide($a, $b),
+        ],
+    ];
+}
+
